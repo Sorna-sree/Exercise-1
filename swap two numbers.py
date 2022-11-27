bc5a107606ef6df44a32437ab6b714ac986a4304
Sorna-sree <110393066+Sorna-sree@users.noreply.github.com>
2022-11-27 20:19:34 +0530

    Add files via upload

---
+++ swap two numbers.py
@@ -0,0 +1,10 @@
+num1=input("Enter your first number: ")
+num2=input("Enter your second number: ")
+temp=''
+print("Before swapping the number: ",num1,num2)
+#swap two numbers
+temp=num1
+num1=num2
+num2=temp
+
+print("After swapping the number:",num1,num2)
