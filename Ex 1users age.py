bbe98c7145acd1e709c9feda878f015203557a31
Sorna-sree <110393066+Sorna-sree@users.noreply.github.com>
2022-11-27 20:23:18 +0530

    Add files via upload

---
+++ Ex 1users age.py
@@ -0,0 +1,8 @@
+name=input("Enter your name")
+born_yr=input("Enter your born year")
+print("Hello ,",name)
+year=2022
+
+#age calculation
+age= int(year)-int(born_yr)
+print(f"you are {age}  Years old")
